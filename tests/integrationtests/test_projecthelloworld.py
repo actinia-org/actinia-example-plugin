f6a20beae77e311980085c9b0bb7b552c1d0dedf
Lina Krisztian <krisztian@mundialis.de>
2025-03-21 08:56:47 +0100

    add test for location and project endpoint for different grass version

--- tests/integrationtests/test_projecthelloworld.py
+++ tests/integrationtests/test_projecthelloworld.py
@@ -33,6 +33,7 @@ from tests.testsuite import ActiniaTestCase
 
 STATUS_CODE_200 = 200
 STATUS_CODE_400 = 400
+STATUS_CODE_404 = 404
 
 
 class ActiniaHelloWorldTest(ActiniaTestCase):
@@ -104,3 +105,45 @@ class ActiniaHelloWorldTest(ActiniaTestCase):
             resp.status_code == STATUS_CODE_400
         ), f"The status code is not {STATUS_CODE_400}"
         assert resp.data == b"Missing name in JSON content"
+
+    @pytest.mark.integrationtest
+    def test_redirecting_deprecated_locations_endpoint(self) -> None:
+        """Test redirecting of deprecated locations to projects endpoint."""
+        if self.grass_version >= [8, 4]:
+            resp = self.app.get(
+                f"{URL_PREFIX}/helloworld/locations/project1",
+            )
+            assert isinstance(
+                resp,
+                Response,
+            ), "The response is not of type Response"
+            assert (
+                resp.status_code == STATUS_CODE_200
+            ), f"The status code is not {STATUS_CODE_200}"
+            resp_location = resp.location.replace("http://localhost", "")
+            assert (
+                resp_location == f"{URL_PREFIX}/helloworld/projects/project1"
+            ), ("The deprecated locations endpoint "
+                "is not forwarded to projects endpoint")
+
+    @pytest.mark.integrationtest
+    def test_projects_endpoint_for_lt_g84(self) -> None:
+        """Test non-supported project endpoint for GRASS versions < g84"""
+        if self.grass_version < [8, 4]:
+            resp = self.app.get(
+                f"{URL_PREFIX}/helloworld/projects/project1",
+            )
+            assert isinstance(
+                resp,
+                Response,
+            ), "The response is not of type Response"
+            assert (
+                resp.status_code == STATUS_CODE_404
+            ), f"The status code is not {STATUS_CODE_404}"
+            assert (
+                resp.json["message"] == (
+                                         "Not Found. The requested URL "
+                                         "is only available from "
+                                         "GRASS GIS version 8.4."
+                                         )
+            ), f"Wrong return message: {resp.data}"
